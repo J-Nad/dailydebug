605165870dc402401b72045b5adfe3640de03568
Jovan Nadendla <91028790+J-Nad@users.noreply.github.com>
2026-01-08 14:27:07 -0600

    Add files via upload

---
+++ supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+
+export const supabase = createClient(
+  "https://qdakoglfjniumlvtuduh.supabase.com",
+  "sb_publishable_88kKPthqrfkB3F2PZLDXlA_HRdeQaqK"
+);
