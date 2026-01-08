f542f2b4eff4bf5078242732c9719bef9ad54d47
Jovan Nadendla <91028790+J-Nad@users.noreply.github.com>
2026-01-08 14:39:39 -0600

    Update Supabase client with new JWT token
    Updated the Supabase client initialization with a new JWT token.

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,6 +2,7 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 export const supabase = createClient(
   "https://qdakoglfjniumlvtuduh.supabase.co",
-  "sb_publishable_88kKPthqrfkB3F2PZLDXlA_HRdeQaqK"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkYWtvZ2xmam5pdW1sdnR1ZHVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc5MDI2OTIsImV4cCI6MjA4MzQ3ODY5Mn0.E0YXv0pMklrgvj0vmvvnrTT8kzAdYWsNTIKznFvtsYg"
 );
+
 
